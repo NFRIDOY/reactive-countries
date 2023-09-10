6afdf02db785fd0cbd132ac8610242b2dc0016ed
NF RIDOY <nfridoy@gmail.com>
2023-09-10 20:41:20 +0600

    countries API

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+import { useEffect, useState } from 'react'
+export default function Countries() {
+    const [countries, setCountries] = useState([])
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        const URL = "https://restcountries.com/v3.1/all";
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => setCountries(data))
+        // countries.forEach(c => {
+        // })
+        console.log(countries);
+        
+    }, [])
+    // }, [count])
+
+    const showCountry = () => {
+        setCount(count+1)
+    }
+    return (
+        <div onClick={showCountry}>Countries: {countries.length} </div>
+    )
+}
